dd96e9457c1da1dd52ef988154869dcb969ebd55
i-m-young <youngisaiahm@gmail.com>
2026-04-20 11:19:39 -0700

    Added HTML from API

--- app.js
+++ app.js
@@ -2,3 +2,31 @@
 // SHOW 1ST 6 MOVIES + IMAGES
 // ADD SORT FUNCTION
 // ADD SKELETON LOADING STATE
+
+const userListEl = document.querySelector(".user-list");
+
+async function main() {
+  const movies = await fetch("https://www.omdbapi.com/?apikey=add5cb2b&s=fast");
+  const moviesData = await movies.json();
+
+  const movieList = moviesData.Search.slice(0, 6);
+
+  userListEl.innerHTML = movieList.map((movie) => movieHTML(movie)).join("");
+}
+
+main();
+
+function movieHTML(movie) {
+  return `<div class="user-card">
+            <div class="user-card__container">
+                <h3>${movie.Title}</h3>
+                <p><b>Year:</b> ${movie.Year}</p>
+                <p><b>imdbID:</b> ${movie.imdbID}</p>
+                <p><b>Poster:</b> 
+                  <a href="${movie.Poster}" target="_blank">
+                    View Poster
+                  </a>
+                </p>
+            </div>
+        </div>`;
+}
