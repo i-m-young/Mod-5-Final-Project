931cd37f71b2ce5b61f1abe23297254a5fbf3b55
i-m-young <youngisaiahm@gmail.com>
2026-04-21 13:59:40 -0700

    Added old/new + a/z filter and footer

--- app.js
+++ app.js
@@ -2,19 +2,39 @@
 // SHOW 1ST 6 MOVIES + IMAGES
 // ADD SORT FUNCTION
 // ADD SKELETON LOADING STATE
+// ADD FOOTER, SORT (A/Z & OLD/NEW)
 
 const userListEl = document.querySelector(".user-list");
 
-async function main() {
-  const movies = await fetch("https://www.omdbapi.com/?apikey=add5cb2b&s=fast");
-  const moviesData = await movies.json();
+let movies = [];
 
-  const movieList = moviesData.Search.slice(0, 6);
-
-  userListEl.innerHTML = movieList.map((movie) => movieHTML(movie)).join("");
+async function getMovies() {
+  const res = await fetch("https://www.omdbapi.com/?apikey=add5cb2b&s=fast");
+  const data = await res.json();
+  return data.Search.slice(0, 6);
 }
 
-main();
+async function renderMovies(filter) {
+  if (!movies.length) {
+    movies = await getMovies();
+  }
+
+  if (filter === "A_TO_Z") {
+    movies.sort((a, b) => a.Title.localeCompare(b.Title));
+  } else if (filter === "Z_TO_A") {
+    movies.sort((a, b) => b.Title.localeCompare(a.Title));
+  } else if (filter === "NEWEST") {
+    movies.sort((a, b) => Number(b.Year) - Number(a.Year));
+  } else if (filter === "OLDEST") {
+    movies.sort((a, b) => Number(a.Year) - Number(b.Year));
+  }
+
+  userListEl.innerHTML = movies.map((movie) => movieHTML(movie)).join("");
+}
+
+function filterMovies(event) {
+  renderMovies(event.target.value);
+}
 
 function movieHTML(movie) {
   return `<div class="user-card">
@@ -22,11 +42,8 @@ function movieHTML(movie) {
                 <h3>${movie.Title}</h3>
                 <p><b>Year:</b> ${movie.Year}</p>
                 <p><b>imdbID:</b> ${movie.imdbID}</p>
-                <p><b>Poster:</b> 
-                  <a href="${movie.Poster}" target="_blank">
-                    View Poster
-                  </a>
-                </p>
+                <img src="${movie.Poster}" alt="${movie.Title} Poster" />
             </div>
         </div>`;
 }
+renderMovies();
